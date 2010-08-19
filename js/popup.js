4a7a001b226e33941d836d0d3ca5c26a72e575d5
Greggory Hernandez <greggory.hz@gmail.com>
2010-08-19 16:54:43 -0600

    start of a complete rewrite

--- js/popup.js
+++ js/popup.js
@@ -16,44 +16,6 @@
     
     Copyright 2010 Greggory Hernandez
 */
-var PasswordGenerator = Class.create({
-	initialize: function () {
-	},
-	getCharList: function () {
-		if ($L.getItem('advanced') == 'true' && $L.getItem('custom').length > 0) {
-		    return $L.getItem('custom');
-		}
-
-		chars = '';
-		if ($L.getItem('uppercase') == undefined || $L.getItem('uppercase') == 'true') {
-			chars += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-		}
-		if ($L.getItem('lowercase') == undefined || $L.getItem('lowercase') == 'true') {
-			chars += 'abcdefghijklmnopqrstuvwxyz';
-		}
-		if ($L.getItem('numbers') == undefined || $L.getItem('numbers') == 'true') {
-			chars += '1234567890';
-		}
-		if ($L.getItem('special') != undefined && $L.getItem('special') == 'true') {
-			chars += '!@#$%^&*()_+-=~|:";\'<>,.?/';
-		}
-		return chars;
-	},
-	generate: function () {
-		chars = this.getCharList();
-		pass = '';
-		len = ($L.getItem('length') == undefined) ? 8 : $L.getItem('length');
-		for (i = 0; i < len; i++) {
-			x = Math.floor(Math.random() * chars.length);
-			pass += chars.charAt(x);
-		}
-	
-		$('password').value = pass;
-		Clipboard.copy(pass);
-		//return pass;
-	}
-});
-
 Clipboard = {}; 
 Clipboard.utilities = {}; 
 Clipboard.utilities.createTextArea = function(value) { 
@@ -71,43 +33,32 @@ Clipboard.copy = function(data) {
     passgen_txt.select();
     document.execCommand('Copy'); 
     document.body.removeChild(passgen_txt); 
-}; 
-// Can't get this to work. See the problem? 
-Clipboard.paste = function() { 
-    var txt = Clipboard.utilities.createTextArea(); 
-    txt.focus(); 
-    document.execCommand('Paste'); 
-    var value = txt.value; 
-    document.body.removeChild(txt); 
-    return value; 
 };
 
-var Magic = Class.create({
-	initialize: function () {
-		this.show_options_link = $('show_options');
-		this.settings = $('settings');
+var Password = new Class({
+	initialize: function (elem, options) {
+		this.text_box = $(elem);
+		
+		this.setOptions(options);
+		this.generate();
 	},
-	toggleOptions: function () {
-		//this.settings.show();
-		//Effect.blindDown(this.settings.identify());
-		if (this.settings.style.display == 'none')
-			Effect.BlindDown(this.settings.identify());
-		//else {
-		//	Effect.BlindUp(this.settings.identify());
-		//	Effect.morph(window, { style: 'height: 100px' });
-			//$$('body')[0].morph('height: 70px');
-			//alert($$('body')[0]);
-			//$$('body')[0].style.height = '70px';
-		//}
+	generate: function () {
+		
+	},
+	setOptions: function (options) {
+		this.options = {}
+		var defaults = {
+			length: 8,
+			upper: true,
+			lower: true,
+			number: true,
+			special: true
+		}
+		
+		this.options.length = 
 	}
 });
 
-Event.observe(window, 'load', function () {
-	var gen = new PasswordGenerator();
-	var magic = new Magic();
-	
-    gen.generate();
-    $('reload').observe('click', gen.generate.bind(gen));
-    
-    magic.show_options_link.observe('click', magic.toggleOptions.bind(magic));
+document.addEvent('domready', function () {
+	var pass = new Password('password');
 });
