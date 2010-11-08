2a32f203fced46311fdc194b0932c240fb1f97db
Greggory Hernandez <greggory.hz@gmail.com>
2010-11-07 22:58:05 -0700

    added email address to license notification

--- js/options.js
+++ js/options.js
@@ -14,7 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with Password Generator.  If not, see <http://www.gnu.org/licenses/>.
     
-    Copyright 2010 Greggory Hernandez
+    Copyright 2010 Greggory Hernandez <greggory.hz@gmail.com>
 */
 
 var OptionsForm = new Class({
